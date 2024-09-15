9d4db4eab2f128474024c358e6896ac213b8065b
paypur <pieceofpaypur@gmail.com>
2024-09-15 17:32:29 -0400

    Removed * Tool parts category, merged upstream
    Took 23 minutes

--- src/main/java/me/paypur/tconjei/jei/TConJEIPlugin.java
+++ src/main/java/me/paypur/tconjei/jei/TConJEIPlugin.java
@@ -11,12 +11,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.materials.MaterialRegistry;
-import slimeknights.tconstruct.library.tools.definition.ToolDefinitionLoader;
-import slimeknights.tconstruct.library.tools.layout.StationSlotLayoutLoader;
 import slimeknights.tconstruct.tables.TinkerTables;
 import slimeknights.tconstruct.tools.stats.*;
 
-import java.util.Comparator;
 import java.util.List;
 
 import static me.paypur.tconjei.TConJEI.MOD_ID;
@@ -28,7 +25,6 @@ public class TConJEIPlugin implements IModPlugin {
     ResourceLocation UID = new ResourceLocation(MOD_ID, "jei_plugin");
     private static final RecipeType<ToolStatsWrapper> HARVEST_STATS = RecipeType.create(MOD_ID, "harvest_stats", ToolStatsWrapper.class);
     private static final RecipeType<ToolStatsWrapper> RANGED_STATS = RecipeType.create(MOD_ID, "ranged_stats", ToolStatsWrapper.class);
-    private static final RecipeType<ToolPartsWrapper> TOOL_PARTS = RecipeType.create(MOD_ID, "tool_parts", ToolPartsWrapper.class);
 
     @NotNull
     @Override
@@ -44,7 +40,6 @@ public class TConJEIPlugin implements IModPlugin {
                 .toList();
         registration.addRecipes(HARVEST_STATS, statsWrappers.stream().filter(w -> w.hasStats(List.of(HeadMaterialStats.ID, ExtraMaterialStats.ID, HandleMaterialStats.ID))).toList());
         registration.addRecipes(RANGED_STATS, statsWrappers.stream().filter(w -> w.hasStats(List.of(LimbMaterialStats.ID, GripMaterialStats.ID, BowstringMaterialStats.ID))).toList());
-        registration.addRecipes(TOOL_PARTS, toolDefinitions());
     }
 
     @Override
@@ -52,24 +47,13 @@ public class TConJEIPlugin implements IModPlugin {
         final IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
         registration.addRecipeCategories(new HarvestStatsCategory(guiHelper));
         registration.addRecipeCategories(new RangedStatsCategory(guiHelper));
-        registration.addRecipeCategories(new ToolPartsCategory(guiHelper));
     }
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
-        registration.addRecipeCatalyst(new ItemStack(TinkerTables.tinkerStation.asItem()), HARVEST_STATS, RANGED_STATS, TOOL_PARTS);
-        registration.addRecipeCatalyst(new ItemStack(TinkerTables.tinkersAnvil.asItem()), HARVEST_STATS, RANGED_STATS, TOOL_PARTS);
-        registration.addRecipeCatalyst(new ItemStack(TinkerTables.scorchedAnvil.asItem()), HARVEST_STATS, RANGED_STATS, TOOL_PARTS);
-    }
-
-    private List<ToolPartsWrapper> toolDefinitions() {
-        return ToolDefinitionLoader.getInstance().getRegisteredToolDefinitions()
-                .stream()
-                .filter(definition -> definition.isMultipart() &&
-                        !definition.getId().equals(new ResourceLocation("tconstruct", "slime_helmet")))
-                .sorted(Comparator.comparingInt(a -> StationSlotLayoutLoader.getInstance().get(a.getId()).getSortIndex()))
-                .map(ToolPartsWrapper::new)
-                .toList();
+        registration.addRecipeCatalyst(new ItemStack(TinkerTables.tinkerStation.asItem()), HARVEST_STATS, RANGED_STATS);
+        registration.addRecipeCatalyst(new ItemStack(TinkerTables.tinkersAnvil.asItem()), HARVEST_STATS, RANGED_STATS);
+        registration.addRecipeCatalyst(new ItemStack(TinkerTables.scorchedAnvil.asItem()), HARVEST_STATS, RANGED_STATS);
     }
 
 }
